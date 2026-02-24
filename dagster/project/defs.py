2785f79b802083ad19e958c43303f65cb33a52d4
tusharpatil-phdata <tpatil@phdata.io>
2026-02-25 03:18:56 +0530

    register Dagster assets and resources in Definitions.

---
+++ dagster/project/defs.py
@@ -0,0 +1,23 @@
+from dagster import Definitions
+from .resources import snowflake_conn, dbt_cloud
+from .assets_ingestion import (
+    kaggle_download_netflix,
+    snowflake_stage_netflix,
+    raw_netflix_titles,
+)
+from .assets_dbt import dbt_poc_run
+from .assets_insights import netflix_year_insights
+
+defs = Definitions(
+    assets=[
+        kaggle_download_netflix,
+        snowflake_stage_netflix,
+        raw_netflix_titles,
+        dbt_poc_run,
+        netflix_year_insights,
+    ],
+    resources={
+        "snowflake_conn": snowflake_conn,
+        "dbt_cloud": dbt_cloud,
+    },
+)
