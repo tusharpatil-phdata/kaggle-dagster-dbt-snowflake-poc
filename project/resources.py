0f472a908f636b1ec124c1dc7f86ae942259d986
tusharpatil-phdata <tpatil@phdata.io>
2026-02-25 05:41:31 +0530

    Update resources.py

--- project/resources.py
+++ project/resources.py
@@ -14,5 +14,8 @@ def snowflake_conn(_):
         database=os.environ["SNOWFLAKE_DATABASE"],
     )
 
-# Let DbtCloudResource read its config (token, account_id, etc.) from env/config
-dbt_cloud = DbtCloudResource()
+# IMPORTANT: pass auth token and account id as POSITIONAL args
+dbt_cloud = DbtCloudResource(
+    os.environ["DBT_CLOUD_API_TOKEN"],          # auth_token
+    int(os.environ["DBT_CLOUD_ACCOUNT_ID"]),    # account_id
+)
