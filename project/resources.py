812e9b904914a635d9f88e2b1316ca73d0116dff
tusharpatil-phdata <tpatil@phdata.io>
2026-02-25 06:08:55 +0530

    remove DbtCloudResource and keep only snowflake_conn resource

--- project/resources.py
+++ project/resources.py
@@ -1,7 +1,6 @@
 import os
 import snowflake.connector
 from dagster import resource
-from dagster_dbt import DbtCloudResource
 
 @resource
 def snowflake_conn(_):
@@ -9,13 +8,6 @@ def snowflake_conn(_):
         account=os.environ["SNOWFLAKE_ACCOUNT"],
         user=os.environ["SNOWFLAKE_USER"],
         password=os.environ["SNOWFLAKE_PASSWORD"],
-        role=os.environ["SNOWFLAKE_ROLE"],
         warehouse=os.environ["SNOWFLAKE_WAREHOUSE"],
         database=os.environ["SNOWFLAKE_DATABASE"],
     )
-
-# IMPORTANT: pass auth token and account id as POSITIONAL args
-dbt_cloud = DbtCloudResource(
-    os.environ["DBT_CLOUD_API_TOKEN"],          # auth_token
-    int(os.environ["DBT_CLOUD_ACCOUNT_ID"]),    # account_id
-)
